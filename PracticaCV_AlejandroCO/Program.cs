c237f2ff313673b7033d4b7f680c828b86339b62
itsalex39 <alexcanor8866@gmail.com>
2026-02-18 08:38:52 +0100

    Plantilla para organizar el codigo

--- PracticaCV_AlejandroCO/Program.cs
+++ PracticaCV_AlejandroCO/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Constantes
+
+            //Variables
+
+            //Inicializacion
+
+            //Entrada
+
+            //Proceso
+
+            //Salida
         }
     }
 }
